689b37122cee87496cbf27639abdbfdb61f07538
chethanMysore <chethan.mr@happiestminds.com>
2017-04-13 14:05:00 +0530

    Folder creation

---
+++ Scripts/AngularFrontEndFactory.js
@@ -0,0 +1,13 @@
+var app = angular.module('AngularModule',[]);
+app.factory('crudService', function (apiFactory) {
+    return {
+        onPost: function(uri,data){
+        	apiFactory.onApiCall(uri,data).then(function(res){
+        		return res;
+        	},function(err){
+        		return err;
+        	});
+        	
+        }
+    }
+});
