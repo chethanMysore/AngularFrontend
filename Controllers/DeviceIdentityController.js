2f45ca1936f62f752e59f323155846644cd5e215
sushanthr7 <sushanth.r@happiestminds.com>
2017-04-13 20:12:17 +0530

    Controller for Device Identity

---
+++ Controllers/DeviceIdentityController.js
@@ -0,0 +1,114 @@
+var app = angular.module('AngularModule',[]);
+
+var DeviceIdentityController = function($scope,apiDispatcher,uriFactory,$location,$cookieStore){
+	
+	$scope.getDevices = function(){
+		$scope.devices = [];
+		var url = uriFactory.uri.getAllDevices;
+		apiDispatcher.getAll(url)
+			.then(function(response){
+				angular.forEach(response.data,function(value,key){
+					$scope.devices.push({
+						id: value.id,
+						DeviceId: value.DeviceId,
+						DeviceKey: value.DeviceKey,
+						IOTHubEndpoint: value.IOTHubEndpoint,
+						SerialNumber: value.SerialNumber,
+						MACAddress: value.MACAddress,
+						BLEUUID: value.BLEUUID,
+						AccountID: value.AccountID,
+						SystemID: value.SystemID,
+						ConfigurationStoreURL: value.ConfigurationStoreURL,
+						Lastupdatedconfiguration: value.Lastupdatedconfiguration,
+						DeviceFirmwareVersion: value.DeviceFirmwareVersion
+
+					});
+				});
+			},function(){});
+	}
+	
+	$scope.showDevice = function(index){
+		$location.path('/device');
+		$cookieStore.put('deviceId',$scope.devices[index].SerialNumber);
+	}
+	
+	$scope.getDeviceById = function(){
+		
+		var url = uriFactory.uri.getDeviceById + $cookieStore.get('deviceId');
+		apiDispatcher.getById(url)
+			.then(function(response){
+				var value = response.data;
+				$scope.device.push({
+						id: value.id,
+						DeviceId: value.DeviceId,
+						DeviceKey: value.DeviceKey,
+						IOTHubEndpoint: value.IOTHubEndpoint,
+						SerialNumber: value.SerialNumber,
+						MACAddress: value.MACAddress,
+						BLEUUID: value.BLEUUID,
+						AccountID: value.AccountID,
+						SystemID: value.SystemID,
+						ConfigurationStoreURL: value.ConfigurationStoreURL,
+						Lastupdatedconfiguration: value.Lastupdatedconfiguration,
+						DeviceFirmwareVersion: value.DeviceFirmwareVersion
+				});
+			},function(){
+				
+			});
+	}
+	
+	$scope.addDevice = function(){
+		var url_deviceIdentity = uriFactory.uri.createDevice;
+		var url_deviceIdentity = uriFactory.uri.createDeviceDirectory;
+		var data = {
+						id: value.id,
+						DeviceId: value.DeviceId,
+						DeviceKey: value.DeviceKey,
+						IOTHubEndpoint: value.IOTHubEndpoint,
+						SerialNumber: value.SerialNumber,
+						MACAddress: value.MACAddress,
+						BLEUUID: value.BLEUUID,
+						AccountID: value.AccountID,
+						SystemID: value.SystemID,
+						ConfigurationStoreURL: value.ConfigurationStoreURL,
+						Lastupdatedconfiguration: value.Lastupdatedconfiguration,
+						DeviceFirmwareVersion: value.DeviceFirmwareVersion
+		};
+		apiDispatcher.postData(url_deviceIdentity,data)
+			.then(function(response){
+				apiDispatcher.postData(url_deviceIdentity,data)
+					.then(function(res){
+						
+					},function(){
+						
+					});
+			},function(){
+				
+			});
+	
+};
+
+	$scope.updateDevice = function(){
+		var url = uriFactory.uri.updateDevice;
+		var data = {
+						id: value.id,
+						DeviceId: value.DeviceId,
+						DeviceKey: value.DeviceKey,
+						IOTHubEndpoint: value.IOTHubEndpoint,
+						SerialNumber: value.SerialNumber,
+						MACAddress: value.MACAddress,
+						BLEUUID: value.BLEUUID,
+						AccountID: value.AccountID,
+						SystemID: value.SystemID,
+						ConfigurationStoreURL: value.ConfigurationStoreURL,
+						Lastupdatedconfiguration: value.Lastupdatedconfiguration,
+						DeviceFirmwareVersion: value.DeviceFirmwareVersion
+		};
+		apiDispatcher.postData(url,data)
+			.then(function(response){
+				
+			},function(){
+			});
+	};
+
+app.controller('DeviceIdentityController',DeviceIdentityController);
--- Scripts/uriService.js
+++ Scripts/uriService.js
@@ -19,7 +19,7 @@ app.factory('uriFactory',function(){
 	uri.updateSystem = "http://ice-test-coreservicesapi.azurewebsites.net/api/System";
 	uri.deleteSystem = "http://ice-test-coreservicesapi.azurewebsites.net/api/System?systemId=";
 	uri.getAllDevices = 'http://ice-test-coreservicesapi.azurewebsites.net/api/DeviceIdentity';
-	uri.getDeviceById = 'http://ice-test-coreservicesapi.azurewebsites.net/api/DeviceIdentity?serialNumber';//have to pass the Serial Number as parameter.
+	uri.getDeviceById = 'http://ice-test-coreservicesapi.azurewebsites.net/api/DeviceIdentity?serialNumber=';//have to pass the Serial Number as parameter.
 	uri.createDevice = 'http://ice-test-coreservicesapi.azurewebsites.net/api/DeviceIdentityIOT';
 	uri.createDeviceDirectory = 'http://ice-test-coreservicesapi.azurewebsites.net/api/DeviceIdentityIOT';
 	uri.updateDevice ='http://ice-test-coreservicesapi.azurewebsites.net/api/DeviceIdentity';//using POST method
