79b9df43c7be31fd6fc456ab91ff49f6fd702d25
chethanMysore <chethan.mr@happiestminds.com>
2017-04-17 16:11:28 +0530

    Added alerts to System controller

--- Controllers/SystemsController.js
+++ Controllers/SystemsController.js
@@ -1,12 +1,12 @@
 
-var SystemsController = function (uriFactory, $scope, $location, $cookieStore, apiDispatcher, loginService) {
+var SystemsController = function (uriFactory, $scope, $location, $cookieStore, apiDispatcher, loginService,ngToast) {
     $scope.post = false;
     $scope.update = false;
     $scope.delete = false;
     $scope.list = false;
     $scope.getAllSystems = function () {
         $scope.Devices = {};
-        alert('hi');
+       
         console.log(uriFactory.getToken);
         console.log("inside getAllSystems");
         loginService.doLogin(uriFactory.getToken).then(function (res) {
@@ -25,10 +25,22 @@ var SystemsController = function (uriFactory, $scope, $location, $cookieStore, a
                     console.log("System data is pushed inside RetrievedData1");
                 });
             }, function () {
-
+                ngToast.create({
+                    className: 'success',
+                    content: '<p>Login Successful</p>',
+                    dismissOnTimeout: true,
+                    timeout: 4000,
+                    dismissOnClick: true
+                });
             });
         }, function () {
-            $location.path('/Home');
+            ngToast.create({
+                className: 'warning',
+                content: '<p>Oops!!! Something went wrong! please try again after sometime</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
         });
     }
 
@@ -40,10 +52,62 @@ var SystemsController = function (uriFactory, $scope, $location, $cookieStore, a
         $scope.Site_Id = "";
         $scope.Account_Id = "";
         $scope.Device_Identity = "";
+        apiDispatcher.getAll(uriFactory.getAllDevices).then(function (response) {
+            angular.forEach(response.data, function (value, key) {
+
+                $scope.RetrievedDevices.push({
+                    deviceId: value.DeviceId,
+                    isChecked: false
+                });
+
+            });
+
+        }, function () {
+            ngToast.create({
+                className: 'warning',
+                content: '<p>Oops!!! Something went wrong! please try again after sometime</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
+        });
     }
 
     $scope.updateClicked = function () {
+        
+
         $scope.update = true;
+        
+        for (var i = 1; i < 13; i++) {
+            document.getElementById("rem" + i).removeAttribute("disabled");
+
+        }
+
+        $scope.RetrievedDevices = [];
+        apiDispatcher.getAll(uriFactory.getAllDevices).then(function (response) {
+            angular.forEach(response.data, function (value, key) {
+                if (value.SystemID == $cookieStore.get('SystemId')) {
+                    $scope.RetrievedDevices.push({
+                        deviceId: value.DeviceId,
+                        isChecked: true
+                    });
+                }
+                else {
+                    $scope.RetrievedDevices.push({
+                        deviceId: value.DeviceId,
+                        isChecked: false
+                    });
+                }
+            });
+        }, function () {
+            ngToast.create({
+                className: 'warning',
+                content: '<p>Oops!!! Something went wrong! please try again after sometime</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
+        });
     }
 
     $scope.deleteClicked = function () {
@@ -51,13 +115,13 @@ var SystemsController = function (uriFactory, $scope, $location, $cookieStore, a
     }
 
     $scope.showCheckboxes = function () {
-        //alert('hi');
+        
         $scope.list = !$scope.list;
 
     }
 
     $scope.showSystem = function (index) {
-        //$location.path('/Accounts');
+       
         $cookieStore.put('SystemId', $scope.RetrievedData1[index].Sysid);
         console.log($scope.RetrievedData1[index]);
 
@@ -68,7 +132,7 @@ var SystemsController = function (uriFactory, $scope, $location, $cookieStore, a
 	      $scope.Site_Id = $scope.RetrievedSystem.Site_Id,
 	     $scope.Account_Id = $scope.RetrievedSystem.Account_Id,
 	     $scope.Device_Identity = $scope.RetrievedSystem.Device_Identity
-        //$scope.getAccountById();
+       
     }
 
     $scope.getSystemById = function () {
@@ -98,61 +162,117 @@ var SystemsController = function (uriFactory, $scope, $location, $cookieStore, a
             });
 
         }, function () {
-
+            ngToast.create({
+                className: 'warning',
+                content: '<p>Oops!!! Something went wrong! please try again after sometime</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
         });
 
-        console.log($scope.RetrievedAccount);
+       
     }
 
-    $scope.reset = function () { }
+    $scope.reset = function () {
+        $scope.post = false;
+        $scope.update = false;
+        $scope.delete = false;
+        $scope.list = false;
+        $scope.RetrievedSystem = [];
+    }
 
     $scope.createSystem = function () {
+        var selected_Devices = [];
+        angular.forEach($scope.RetrievedDevices, function (value, key) {
+            if (value.isChecked) {
+                selected_Devices.push(value.deviceId);
+            }
+        })
         var data = {
             id: $scope.id,
             system_Name: $scope.System_Name,
             system_Location: $scope.System_Location,
             site_Id: $scope.Site_Id,
-            account_Id: $scope.Account_Id
+            account_Id: $scope.Account_Id,
+            devices: selected_Devices
         };
         apiDispatcher.postData(uriFactory.createSystem, data).then(function (res) {
-
+            ngToast.create({
+                className: 'success',
+                content: '<p>System Added Successfully</p>',
+                dismissOnTimeout: true,
+                timeout: 2000,
+                dismissOnClick: true
+            });
         }, function () {
-
+            ngToast.create({
+                className: 'warning',
+                content: '<p>System Adding Failed! Please try again later</p>',
+                dismissOnTimeout: true,
+                timeout: 2000,
+                dismissOnClick: true
+            });
         });
     }
 
     $scope.updateSystem = function () {
+        var selected_Devices = [];
+        angular.forEach($scope.RetrievedDevices, function (value, key) {
+            if (value.isChecked) {
+                selected_Devices.push(value.deviceId);
+            }
+        })
         var data = {
             id: $scope.System_id,
             system_Name: $scope.System_Name,
             system_Location: $scope.System_Location,
             site_Id: $scope.Site_Id,
-            account_Id: $scope.Account_Id
+            account_Id: $scope.Account_Id,
+            devices: selected_Devices
         };
         apiDispatcher.update(uriFactory.updateSystem, data).then(function (res) {
-
+            ngToast.create({
+                className: 'success',
+                content: '<p>System Updated Successfully</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
         }, function () {
-
+            ngToast.create({
+                className: 'danger',
+                content: '<p>Oops!!! Update Failed! Change a few things and try again</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
         });
     }
 
     $scope.deleteSystem = function () {
         apiDispatcher.deleteById(uriFactory.deleteSystem + $cookieStore.get('SystemId')).then(function (res) {
-
+            ngToast.create({
+                className: 'success',
+                content: '<p>System Deleted Successfully</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
         }, function (res) {
-
+            ngToast.create({
+                className: 'danger',
+                content: '<p>Oops!!! System cannot be deleted</p>',
+                dismissOnTimeout: true,
+                timeout: 4000,
+                dismissOnClick: true
+            });
         });
     }
 
-    $scope.getAccountInfo = function () {
-        apiDispatcher.getAll(uriFactory.getAccountInfo).then(function (res) {
-
-        }, function () {
-
-        });
-    }
+   
 }
 
 
 
-app.controller('SystemsController', ['uriFactory', '$scope', '$location', '$cookieStore', 'apiDispatcher', 'loginService', SystemsController]);
+app.controller('SystemsController', ['uriFactory', '$scope', '$location', '$cookieStore', 'apiDispatcher', 'loginService', 'ngToast', SystemsController]);
