efe794f005d8bbe4ee87ec20fa41962f1cb23a09
chethanMysore <chethan.mr@happiestminds.com>
2017-04-17 14:31:21 +0530

    Account finalizing

--- Controllers/AccountController.js
+++ Controllers/AccountController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var AccountController = function (uriFactory, $scope, $location, $cookieStore, apiDispatcher, loginService) {
+var AccountController = function (uriFactory, $scope, $location, $cookieStore, apiDispatcher, loginService,ngToast) {
     $scope.post = false;
     $scope.update = false;
     $scope.delete = false;
@@ -44,6 +44,23 @@ var AccountController = function (uriFactory, $scope, $location, $cookieStore, a
     $scope.postClicked = function () {
         $scope.post = true;
         $scope.RetrievedSites = [];
+        
+       
+        $scope.id = "";
+        $scope.Corporate_Name = "";
+        $scope.Corporate_ShortName = "";
+        $scope.Corporate_TradeName = "";
+        $scope.Corporate_Address_Street = "";
+        $scope.Corporate_Address_City = "";
+        $scope.Corporate_Address_State = "";
+        $scope.Corporate_Address_Zip = "";
+        $scope.Corporate_Address_Country = "";
+        $scope.Corporate_Phone_1 = "";
+        $scope.Corporate_Phone_2 = "";
+        $scope.Corporate_Phone_3 = "";
+        $scope.Corporate_Logo = "";
+        $scope.Corporate_ID = "";
+        $scope.Site_ID = "";
 
         apiDispatcher.getAll(uriFactory.getAllSites).then(function (response) {
             angular.forEach(response.data, function (value, key) {
@@ -67,6 +84,28 @@ var AccountController = function (uriFactory, $scope, $location, $cookieStore, a
 
     $scope.updateClicked = function () {
         $scope.update = true;
+        for (var i = 1; i < 13; i++) {
+            document.getElementById("rem" + i).removeAttribute("disabled");
+            //document.getElementById("rem").removeAttribute("disabled");
+        }
+
+        $scope.RetrievedSites = [];
+        apiDispatcher.getAll(uriFactory.getAllSites).then(function (response) {
+            angular.forEach(response.data, function (value, key) {
+                if(value.account_ID == $cookieStore.get('AccountId')){
+                $scope.RetrievedSites.push({
+                    siteId: value.id,
+                    isChecked: true
+                });
+                }
+                else {
+                    $scope.RetrievedSites.push({
+                        siteId: value.id,
+                        isChecked: false
+                    });
+                }
+            });
+        }, function () { });
     }
 
     $scope.deleteClicked = function () {
@@ -79,139 +118,163 @@ var AccountController = function (uriFactory, $scope, $location, $cookieStore, a
 
     }
 
-	$scope.showAccount = function(index){
-		//$location.path('/Accounts');
-	    $cookieStore.put('AccountId', $scope.RetrievedData[index].id);
-	    console.log($scope.RetrievedData[index]);
-        
-	    $scope.RetrievedAccount = $scope.RetrievedData[index];
-	    $scope.id = $scope.RetrievedAccount.id;
-	    $scope.Corporate_Name = $scope.RetrievedAccount.Corporate_Name,
-	    $scope.Corporate_ShortName = $scope.RetrievedAccount.Corporate_ShortName,
-	    $scope.Corporate_TradeName = $scope.RetrievedAccount.Corporate_TradeName,
-	    $scope.Corporate_Address_Street = $scope.RetrievedAccount.Corporate_Address_Street,
-	    $scope.Corporate_Address_City = $scope.RetrievedAccount.Corporate_Address_City,
-	    $scope.Corporate_Address_State = $scope.RetrievedAccount.Corporate_Address_State,
-	    $scope.Corporate_Address_Zip = $scope.RetrievedAccount.Corporate_Address_Zip,
-	    $scope.Corporate_Address_Country = $scope.RetrievedAccount.Corporate_Address_Country,
-	    $scope.Corporate_Phone_1 = $scope.RetrievedAccount.Corporate_Phone_1,
-	    $scope.Corporate_Phone_2 = $scope.RetrievedAccount.Corporate_Phone_2,
-	    $scope.Corporate_Phone_3 = $scope.RetrievedAccount.Corporate_Phone_3,
-	    $scope.Corporate_Logo = $scope.RetrievedAccount.Corporate_Logo,
-	    $scope.Corporate_ID = $scope.RetrievedAccount.Corporate_ID,
-        $scope.Site_ID = $scope.RetrievedAccount.Site_ID
-	    //$scope.getAccountById();
-	}	
+    $scope.showAccount = function (index) {
+        //$location.path('/Accounts');
+        $cookieStore.put('AccountId', $scope.RetrievedData[index].id);
+        console.log($scope.RetrievedData[index]);
 
-	$scope.getAccountById = function(){
-		$scope.RetrievedAccount = [];
-		apiDispatcher.getById(uriFactory.getAccountById + $cookieStore.get('AccountId')).then(function (res) {
-		    console.log('inside getAccount');
+        $scope.RetrievedAccount = $scope.RetrievedData[index];
+        $scope.id = $scope.RetrievedAccount.id;
+        $scope.Corporate_Name = $scope.RetrievedAccount.Corporate_Name;
+        $scope.Corporate_ShortName = $scope.RetrievedAccount.Corporate_ShortName;
+        $scope.Corporate_TradeName = $scope.RetrievedAccount.Corporate_TradeName;
+        $scope.Corporate_Address_Street = $scope.RetrievedAccount.Corporate_Address_Street;
+        $scope.Corporate_Address_City = $scope.RetrievedAccount.Corporate_Address_City;
+        $scope.Corporate_Address_State = $scope.RetrievedAccount.Corporate_Address_State;
+        $scope.Corporate_Address_Zip = $scope.RetrievedAccount.Corporate_Address_Zip;
+        $scope.Corporate_Address_Country = $scope.RetrievedAccount.Corporate_Address_Country;
+        $scope.Corporate_Phone_1 = $scope.RetrievedAccount.Corporate_Phone_1;
+        $scope.Corporate_Phone_2 = $scope.RetrievedAccount.Corporate_Phone_2;
+	    $scope.Corporate_Phone_3 = $scope.RetrievedAccount.Corporate_Phone_3;
+	    $scope.Corporate_Logo = $scope.RetrievedAccount.Corporate_Logo;
+	    $scope.Corporate_ID = $scope.RetrievedAccount.Corporate_ID;
+	    $scope.Site_ID = $scope.RetrievedAccount.Site_ID;
+        //$scope.getAccountById();
+    }
+
+    $scope.getAccountById = function () {
+        $scope.RetrievedAccount = [];
+        apiDispatcher.getById(uriFactory.getAccountById + $cookieStore.get('AccountId')).then(function (res) {
+            console.log('inside getAccount');
             console.log(res);
-		    console.log(uriFactory.getAccountById + $cookieStore.get('AccountId'));
-		    console.log(res);
-			var value = res.data;
-			$scope.RetrievedAccount.push({
-				id: value.id,
-				  Corporate_Name: value.Corporate_Name,
-				  Corporate_ShortName: value.Corporate_ShortName,
-				  Corporate_TradeName: value.Corporate_TradeName,
-				  Corporate_Address_Street: value.Corporate_Address_Street,
-				  Corporate_Address_City: value.Corporate_Address_City,
-				  Corporate_Address_State: value.Corporate_Address_State,
-				  Corporate_Address_Zip: value.Corporate_Address_Zip,
-				  Corporate_Address_Country: value.Corporate_Address_Country,
-				  Corporate_Phone_1: value.Corporate_Phone_1,
-				  Corporate_Phone_2: value.Corporate_Phone_2,
-				  Corporate_Phone_3: value.Corporate_Phone_3,
-				  Corporate_Logo: value.Corporate_Logo,
-				  Corporate_ID: value.Corporate_ID,
-				  Site_ID: value.Site_ID
-			});
-			
-		},function(){
+            console.log(uriFactory.getAccountById + $cookieStore.get('AccountId'));
+            console.log(res);
+            var value = res.data;
+            $scope.RetrievedAccount.push({
+                id: value.id,
+                Corporate_Name: value.Corporate_Name,
+                Corporate_ShortName: value.Corporate_ShortName,
+                Corporate_TradeName: value.Corporate_TradeName,
+                Corporate_Address_Street: value.Corporate_Address_Street,
+                Corporate_Address_City: value.Corporate_Address_City,
+                Corporate_Address_State: value.Corporate_Address_State,
+                Corporate_Address_Zip: value.Corporate_Address_Zip,
+                Corporate_Address_Country: value.Corporate_Address_Country,
+                Corporate_Phone_1: value.Corporate_Phone_1,
+                Corporate_Phone_2: value.Corporate_Phone_2,
+                Corporate_Phone_3: value.Corporate_Phone_3,
+                Corporate_Logo: value.Corporate_Logo,
+                Corporate_ID: value.Corporate_ID,
+                Site_ID: value.Site_ID
+            });
 
-		});
-		
-		console.log($scope.RetrievedAccount);
-	}
+        }, function () {
 
-	$scope.reset = function () { }
+        });
 
-	$scope.createAccount = function () {
-	    console.log($scope.RetrievedSites);
-	    var selected_Sites = [];
-	    angular.forEach($scope.RetrievedSites, function (value, key) {
-	        if (value.isChecked){
-	            selected_Sites.push(value.siteId);
-	        }
-	    })
-	    console.log(selected_Sites);
-	    console.log($scope.Site_ID);
-		var data = {
-				  id: $scope.id,
-				  corporate_Name: $scope.Corporate_Name,
-				  corporate_ShortName: $scope.Corporate_ShortName,
-				  corporate_TradeName: $scope.Corporate_TradeName,
-				  corporate_Address_Street: $scope.Corporate_Address_Street,
-				  corporate_Address_City: $scope.Corporate_Address_City,
-				  corporate_Address_State: $scope.Corporate_Address_State,
-				  corporate_Address_Zip: $scope.Corporate_Address_Zip,
-				  corporate_Address_Country: $scope.Corporate_Address_Country,
-				  corporate_Phone_1: $scope.Corporate_Phone_1,
-				  corporate_Phone_2: $scope.Corporate_Phone_2,
-				  corporate_Phone_3: $scope.Corporate_Phone_3,
-				  corporate_Logo: $scope.Corporate_Logo,
-				  corporate_ID: $scope.Corporate_ID,
-				  Site_ID: selected_Sites
-		};
-		apiDispatcher.postData(uriFactory.createAccount,data).then(function(res){
+        console.log($scope.RetrievedAccount);
+    }
 
-		},function(){
+    $scope.reset = function () {
+        $scope.post = false;
+        $scope.update = false;
+        $scope.delete = false;
+        $scope.list = false;
+        $scope.RetrievedAccount = [];
+    }
 
-		});
-	}
+    $scope.createAccount = function () {
+        console.log($scope.RetrievedSites);
+        var selected_Sites = [];
+        angular.forEach($scope.RetrievedSites, function (value, key) {
+            if (value.isChecked) {
+                selected_Sites.push(value.siteId);
+            }
+        })
+        console.log(selected_Sites);
+        console.log($scope.Site_ID);
+        var data = {
+            id: $scope.id,
+            corporate_Name: $scope.Corporate_Name,
+            corporate_ShortName: $scope.Corporate_ShortName,
+            corporate_TradeName: $scope.Corporate_TradeName,
+            corporate_Address_Street: $scope.Corporate_Address_Street,
+            corporate_Address_City: $scope.Corporate_Address_City,
+            corporate_Address_State: $scope.Corporate_Address_State,
+            corporate_Address_Zip: $scope.Corporate_Address_Zip,
+            corporate_Address_Country: $scope.Corporate_Address_Country,
+            corporate_Phone_1: $scope.Corporate_Phone_1,
+            corporate_Phone_2: $scope.Corporate_Phone_2,
+            corporate_Phone_3: $scope.Corporate_Phone_3,
+            corporate_Logo: $scope.Corporate_Logo,
+            corporate_ID: $scope.Corporate_ID,
+            Site_ID: selected_Sites
+        };
+        apiDispatcher.postData(uriFactory.createAccount, data).then(function (res) {
+            ngToast.create({
+                className: 'success',
+                content: '<p>Account Added Successfully</p>',
+                dismissOnTimeout: true,
+                timeout: 2000,
+                dismissOnClick: true
+            });
+        }, function () {
+            ngToast.create({
+                className: 'warning',
+                content: '<p>Account Adding Failed! Please try again later</p>',
+                dismissOnTimeout: true,
+                timeout: 2000,
+                dismissOnClick: true
+            });
+        });
+    }
 
-	$scope.updateAccount = function(){
-		var data = {
-			 id: $scope.id,
-				  corporate_Name: $scope.Corporate_Name,
-				  corporate_ShortName: $scope.Corporate_ShortName,
-				  corporate_TradeName: $scope.Corporate_TradeName,
-				  corporate_Address_Street: $scope.Corporate_Address_Street,
-				  corporate_Address_City: $scope.Corporate_Address_City,
-				  corporate_Address_State: $scope.Corporate_Address_State,
-				  corporate_Address_Zip: $scope.Corporate_Address_Zip,
-				  corporate_Address_Country: $scope.Corporate_Address_Country,
-				  corporate_Phone_1: $scope.Corporate_Phone_1,
-				  corporate_Phone_2: $scope.Corporate_Phone_2,
-				  corporate_Phone_3: $scope.Corporate_Phone_3,
-				  corporate_Logo: $scope.Corporate_Logo,
-				  corporate_ID: $scope.Corporate_ID,
-				  Site_ID: $scope.Site_ID
-				};
-		apiDispatcher.update(uriFactory.updateAccount,data).then(function(res){
+    $scope.updateAccount = function () {
+        var selected_Sites = [];
+        angular.forEach($scope.RetrievedSites, function (value, key) {
+            if (value.isChecked) {
+                selected_Sites.push(value.siteId);
+            }
+        })
+        var data = {
+            id: $scope.id,
+            corporate_Name: $scope.Corporate_Name,
+            corporate_ShortName: $scope.Corporate_ShortName,
+            corporate_TradeName: $scope.Corporate_TradeName,
+            corporate_Address_Street: $scope.Corporate_Address_Street,
+            corporate_Address_City: $scope.Corporate_Address_City,
+            corporate_Address_State: $scope.Corporate_Address_State,
+            corporate_Address_Zip: $scope.Corporate_Address_Zip,
+            corporate_Address_Country: $scope.Corporate_Address_Country,
+            corporate_Phone_1: $scope.Corporate_Phone_1,
+            corporate_Phone_2: $scope.Corporate_Phone_2,
+            corporate_Phone_3: $scope.Corporate_Phone_3,
+            corporate_Logo: $scope.Corporate_Logo,
+            corporate_ID: $scope.Corporate_ID,
+            Site_ID: selected_Sites
+        };
+        apiDispatcher.update(uriFactory.updateAccount, data).then(function (res) {
 
-		},function(){
+        }, function () {
 
-		});
-	}
+        });
+    }
 
-	$scope.deleteAccount = function(){
-		apiDispatcher.deleteById(uriFactory.deleteAccount + $cookieStore.get('AccountId')).then(function(res){
+    $scope.deleteAccount = function () {
+        apiDispatcher.deleteById(uriFactory.deleteAccount + $cookieStore.get('AccountId')).then(function (res) {
 
-		},function(res){
+        }, function (res) {
 
-		});
-	}
+        });
+    }
 
-	$scope.getAccountInfo = function(){
-		apiDispatcher.getAll(uriFactory.getAccountInfo).then(function(res){
+    $scope.getAccountInfo = function () {
+        apiDispatcher.getAll(uriFactory.getAccountInfo).then(function (res) {
 
-		},function(){
+        }, function () {
 
-		});
-	}
+        });
+    }
 }
 
-app.controller('AccountController', ['uriFactory', '$scope', '$location', '$cookieStore', 'apiDispatcher', 'loginService', AccountController]);
+app.controller('AccountController', ['uriFactory', '$scope', '$location', '$cookieStore', 'apiDispatcher', 'loginService', 'ngToast', AccountController]);
